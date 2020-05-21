6c7b56020e53f245711c35bdb5cba5f53e12af5b
selby.maluleke <selby.maluleke@CPX-XBQG7D8JS3U.dir.svc.accenture.com>
2020-05-21 04:41:18 +0200

    testing component

--- sm-library/src/app/app.component.ts
+++ sm-library/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {SmFormGroup} from './core/components/form-controls/sm-form-group';
 
@@ -7,11 +7,20 @@ import {SmFormGroup} from './core/components/form-controls/sm-form-group';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-
+export class AppComponent  implements OnInit{
+//testing component with radom data
 isLoading = false;
 isDisabled=false;
 form:FormGroup;
+
+constructor(private formBuilder: FormBuilder) { }
+
+ngOnInit() {
+  this.form = this.formBuilder.group({
+    'testText': [null, [Validators.required]],
+  
+  });
+}
 
 submit() {
   this.isLoading = true;
